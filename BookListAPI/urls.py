88876d39d9a06645428c7d4b4566bccf0db1c6da
Christian David Manga Arrázola <chrisdma17@gmail.com>
2025-02-19 10:39:45 -0500

    first commit

---
+++ BookListAPI/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    # path('books/', views.books)
+    path('books/', views.BookList.as_view()),
+    path('books/<int:pk>/', views.Book.as_view()),
+    path('menu-items/', views.MenuItemsView.as_view()),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('__debug__/', include('debug_toolbar.urls')),
+]
